5a60e98815c176041a7eebc4bed85edc443fc196
harady <harady15@gmail.com>
2016-01-11 06:25:46 +0900

    AssetBundle Variants　試してみる。　どうやら、高解像度用とか低解像度用とかに、アセットバンドルを使い分けるのに使用するようだ。

---
+++ Assets/Editor/GetAssetBundleNames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class GetAssetBundleNames
+{
+	[MenuItem ("Test/GetAssetBundleNames")]
+	static void GetNames ()
+	{
+		var nameList = AssetDatabase.GetAllAssetBundleNames ().ToList ();
+		nameList.ForEach (aName => Debug.Log ("AssetBundle: " + aName));
+	}
+}
