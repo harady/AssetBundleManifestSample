804ff3fedfc85b8b70173efca3d9687bb3ba941f
harady <harady15@gmail.com>
2016-01-11 05:57:53 +0900

    ビルドするエディタスクリプトを作成。

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class AssetBundleBuilder
+{
+	[MenuItem ("Test/BuildAssetBundles")]
+	public static void BuildAssetBundles ()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles");
+	}
+}
